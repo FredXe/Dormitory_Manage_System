f1d4a208985535b5d7a4cd8263f17e9b074c2497
FredXe <102457236+FredXe@users.noreply.github.com>
2023-01-04 02:34:48 +0800

    feat(model/user): add delete()
    docs:
    1. [model/user] rearrange the function declare
    2. [model/user] comment the parameter types

--- app/model/user.js
+++ app/model/user.js
@@ -8,7 +8,7 @@ const Hash = require("./hash");
 /**
  * Convert Rows into Object with JSON's 
  * static functions
- * @param {*} rows Rows input
+ * @param {RawData} rows Rows input
  * @returns Object that `rows` expressed
  */
 function decodeRows(rows) {
@@ -17,14 +17,14 @@ function decodeRows(rows) {
 
 /**
  * SELECT password of specific user
- * @param {*} UserID 
- * @param {*} callback function (`err`, `rows`)
+ * @param {string} UserID 
+ * @param {(err, rows)} callback 
  */
 function getPasswd(UserID, callback) {
-	const queryString = `SELECT password FROM users WHERE UserID = '${UserID}'`;
+	const query = `SELECT password FROM users WHERE UserID = '${UserID}'`;
 
 	// Function that insert User to specific User Type
-	Connections.admin.query(queryString, callback);
+	Connections.admin.query(query, callback);
 }
 
 class User {
@@ -32,9 +32,9 @@ class User {
 	static Admin = {
 		/**
 		 * Insert the Data into TABLE `Users`
-		 * @param {*} UserID Account (required)
-		 * @param {*} Password (required)
-		 * @returns Error message is filled on error
+		 * @param {string} UserID Account (required)
+		 * @param {string} Password Plain text (required)
+		 * @param {(err, rows)} callback 
 		 */
 		regist: function ({
 			UserID,
@@ -55,68 +55,84 @@ class User {
 				return;
 			}
 
+			/**
+			 * Declare query callback function
+			 */
+			// Insert User to specific User Type
+			function insertUserType(err, rows) {
+				if (err) {
+					callback(err.message, rows);
+					return;
+				}
+
+				// INSERT User into specific usertype TABLE
+				const query = `INSERT INTO \`${userType}\`
+									VALUES ('${UserID}'); `;
+
+				Connections.admin.query(query, (err, rows) => {
+					if (err) {
+						callback(err.message, rows);
+						return;
+					}
+					callback(err, rows);
+				});
+			}
+
+			/**
+			 * Execute the function
+			 */
 			Hash.hashPasswd(Password, function (err, hash) {
 				if (err) {
 					callback(err.message);
 					return;
 				}
-
 				// Insert data into `Users`
-				const queryString = `INSERT INTO \`users\` (UserID, Password, name, email, phnumber, sex, eroll_year)
-						VALUES ('${UserID}', '${hash}', '${name}', '${email}', 
-						'${phnumber}', '${sex}', ${eroll_year}); `;
-
-				// Function that insert User to specific User Type
-				function insertUserType(err, rows) {
-					if (err) {
-						callback(err.message, rows);
-						return;
-					}
-
-					// INSERT User into specific usertype TABLE
-					const queryString = `INSERT INTO \`${userType}\`
-										VALUES ('${UserID}'); `;
-
-					Connections.admin.query(queryString, (err, rows) => {
-						if (err) {
-							callback(err.message, rows);
-							return;
-						}
-						callback(err, rows);
-					});
-				}
+				const query = `INSERT INTO \`users\` (UserID, Password, name, email, phnumber, sex, eroll_year)
+					VALUES ('${UserID}', '${hash}', '${name}', '${email}', 
+					'${phnumber}', '${sex}', ${eroll_year}); `;
 
 				// Send SQL query to DB
-				Connections.admin.query(queryString, insertUserType);
+				Connections.admin.query(query, insertUserType);
 			});
 
+		},
 
+		/**
+		 * Delete the User
+		 * NOTE: this function will not check the
+		 * privilege or password.
+		 * @param {string} account 
+		 * @param {(err, rows)} callback `rows.affectRows` == 0
+		 * if no account DELETED
+		 */
+		delete: function (account, callback) {
+			const query = `DELETE FROM users WHERE UserID='${account}'`;
+
+			Connections.admin.query(query, callback);
 		}
 	}
 
 	/**
-	 * Check if the account is inside the
-	 * Database.
-	 * @param {*} account UserID in Database
-	 * @param {*} password Plain text password
-	 * @returns `undefined` if no such user or
-	 * password doesn't match
+	 * Login function
+	 * @param {string} account UserID in Database
+	 * @param {string} password Plain text password
+	 * @param {(err, rows)} callback `rows` is filled
+	 * with privilege if login success, `undefined`
+	 * if no such user or the password doesn't match.
 	 */
 	static login({ account, password }, callback) {
 
-		/**
-		 * SELECT the UserID which matches the input,
-		 * using switch to return the user's previlege
-		 * if the user is inside the table. 
-		 */
-		const queryString = `SELECT U.password, (CASE WHEN U.UserID IN (SELECT UserID FROM \`student\`) THEN 'student'
+		// SELECT the password and the privileges
+		const query = `SELECT U.password, (CASE WHEN U.UserID IN (SELECT UserID FROM \`student\`) THEN 'student'
 				WHEN U.UserID IN (SELECT UserID FROM \`houseMaster\`) THEN 'houseMaster'
 				WHEN U.UserID IN (SELECT UserID FROM \`admin\`) THEN 'admin'
 				ELSE 'Unknown' END) AS privilege FROM \`users\` AS U LEFT JOIN \`student\` AS S ON U.\`UserID\` = S.\`UserID\`
 				WHERE U.\`UserID\`='${account}';`;
 
-		// SELECT the password and the privileges and  
-		Connections.admin.query(queryString, function (err, rows) {
+		/**
+		 * Declare the query callbcak function
+		 */
+		function checkPasswd(err, rows) {
 			if (err) {
 				callback(err.message, rows);
 				return;
@@ -147,7 +163,12 @@ class User {
 				}
 
 			});
-		});
+		}
+
+		/**
+		 * Execute the query
+		 */
+		Connections.admin.query(query, checkPasswd);
 
 	}
 
--- app/test.js
+++ app/test.js
@@ -1,7 +1,7 @@
 /**
  * File for testing
  */
-const route = require("./route/route");
+// const route = require("./route/route");
 const User = require("./model/user");
 const Connections = require("./model/connections");
 
@@ -11,22 +11,26 @@ function handler(err, rows) {
 		console.error(err);
 	}
 	console.log(rows);
+	Connections.close();
+
 }
 
-User.Admin.regist({
-	UserID: 'a1095532', Password: 'qwe', name: 'Fred', eroll_year: 2019, userType: "student"
-}, (err, rows) => {
-	if (err) {
-		console.error(err);
-	}
-	console.log("Inserted: ", rows);
-});
+// User.Admin.delete("a1095532", handler);
+
+// User.Admin.regist({
+// 	UserID: 'a1095532', Password: 'qwe', name: 'Fred', eroll_year: 2019, userType: "student"
+// }, (err, rows) => {
+// 	if (err) {
+// 		console.error(err);
+// 	}
+// 	console.log("Inserted: ", rows);
+// });
 
 // User.login({ account: "a1095532", password: "test" }, handler);
 User.login({ account: "a1095532", password: "qwe" }, handler);
 
-setTimeout(() => {
-	Connections.close();
-}, 1000);
+// setTimeout(() => {
+// 	Connections.close();
+// }, 1000);
 
 
