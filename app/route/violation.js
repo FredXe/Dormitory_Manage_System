84ad1ef618c0361e1ba39400614dcf3e8cbe74e6
Chenghuexin <cindy2090.cheng@gmail.com>
2023-01-08 13:32:05 +0800

    modified violation , board

--- app/route/violation.js
+++ app/route/violation.js
@@ -14,7 +14,7 @@ router.get("/", function (req, res) {
 router.get("/list", function (req, res) {
 	token.decode(req.cookies.token, _selectRecord)
 
-	function _selectRecord() {
+	function _selectRecord(decode) {
 		var account;
 		if (["houseMaster", "admin"].includes(decode.privilege)) {
 			account = "%";
@@ -22,7 +22,7 @@ router.get("/list", function (req, res) {
 			account = decode.account;
 		}
 		violation.showRecord(account, function (err, records) {
-			res.render("student", { records });
+			res.render("violation", { records });
 		})
 	}
 })
@@ -34,7 +34,7 @@ router.post("/insert", function (req, res) {
 
 	function _insertRecord(decode) {
 		violation.insertRecord(decode.account, record.boarder, record.type, function (err, rows) {
-			res.render("/violation/list");
+			res.redirect("/violation/list");
 		});
 	}
 })
